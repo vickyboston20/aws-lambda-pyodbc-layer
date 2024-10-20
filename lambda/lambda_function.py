f8b8f634440d6116f68a18ec933f353d869d5c2c
jenkins <jenkins@prft.com>
2024-10-20 14:06:13 +0530

    added 3.12 version

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,16 +1,17 @@
 import json
+import os
 import pyodbc
 
 def lambda_handler(event, context):
     msodbc_version = event.get('msodbc_version', '17')
-
+    mssql_password = os.getenv('MSSQL_SA_PASSWORD', 'yourStrong(@)Password')
     try:
         conn = pyodbc.connect(
             f'DRIVER={{ODBC Driver {msodbc_version} for SQL Server}};'
             'SERVER=mssql;'
             'DATABASE=master;'
             'UID=sa;'
-            'PWD=yourStrong(@)Password;'
+            f'PWD={mssql_password};'
             'TrustServerCertificate=yes;'
             'Encrypt=yes;'
         )
